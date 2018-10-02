5ec17de99c7ef89bfd59f2d000d9db4829c0f41e
吳姿萱 <cherry51120@gmail.com>
2018-10-02 23:14:56 +0800

    create home page

--- js/public.js
+++ js/public.js
@@ -6,9 +6,15 @@
  */
  
 $(function () {
-  $("#cover-photo").imgLiquid();
+  if($("#cover-photo").length > 0)
+    $("#cover-photo").imgLiquid();
 
   $('#search').click(function() {
     $(this).toggle('active');
+  });
+
+  $('.items > a , .menu > a').click(function() {
+    console.log('test');
+    $(this).toggle('active').siblings().removeClass('active');
   });
 });
